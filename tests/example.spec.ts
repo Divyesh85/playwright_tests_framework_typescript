c03e8fd6081c9ab9b5aa68f264f26817e167fcff
Divyesh Tandel <divyeshtandel1985@gmail.com>
2023-07-31 17:14:32 +0200

    chore:remove commented code

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,34 +1,4 @@
 import { test, expect } from '@playwright/test';
-
-// test.describe("navigation", () => {
-//   test.beforeEach(async ({ page }) => {
-//     // Go to the starting url before each test.
-//     await page.goto("https://playwright.dev/");
-//   });
-
-//   test("main navigation", async ({ page }) => {
-//     // Assertions use the expect API.
-//     await expect(page).toHaveURL("https://playwright.dev/");
-//   });
-
-//   test('has title', async ({ page }) => {
-//     //await page.goto('https://playwright.dev/');
-  
-//     // Expect a title "to contain" a substring.
-//     await expect(page).toHaveTitle(/Playwright/);
-//   });
-
-//   test('get started link', async ({ page }) => {
-//     //await page.goto('https://playwright.dev/');
-  
-//     // Click the get started link.
-//     await page.getByRole('link', { name: 'Get started' }).click();
-  
-//     // Expects the URL to contain intro.
-//     await expect(page).toHaveURL(/.*intro/);
-//   });
-// });
-
 test.describe("nmbrs prod envs", () => {
 test('node 2', async ({ page }) => {
   await page.goto('https://node2.nmbrs.nl/applications/Common/Login.aspx?ReturnUrl=%2f');
